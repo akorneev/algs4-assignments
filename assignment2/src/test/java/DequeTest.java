e161c268f49461f711bf833c20e7288eccb24216
Alexander Korneev <akorneev@gmail.com>
2014-10-02 05:25:24 +0400

    Replace explicit types with <>

--- assignment2/src/test/java/DequeTest.java
+++ assignment2/src/test/java/DequeTest.java
@@ -12,25 +12,25 @@ public class DequeTest {
     public static class EmptyDequeTest {
         @Test
         public void itIsEmpty() {
-            Deque<String> deque = new Deque<String>();
+            Deque<String> deque = new Deque<>();
             assertTrue(deque.isEmpty());
         }
 
         @Test
         public void itHas0Elements() {
-            Deque<String> deque = new Deque<String>();
+            Deque<String> deque = new Deque<>();
             assertEquals(0, deque.size());
         }
 
         @Test(expected = NoSuchElementException.class)
         public void removeFirstThrowsException() {
-            Deque<String> deque = new Deque<String>();
+            Deque<String> deque = new Deque<>();
             deque.removeFirst();
         }
 
         @Test(expected = NoSuchElementException.class)
         public void removeLastThrowsException() {
-            Deque<String> deque = new Deque<String>();
+            Deque<String> deque = new Deque<>();
             deque.removeLast();
         }
     }
@@ -38,21 +38,21 @@ public class DequeTest {
     public static class EmptyDequeIteratorTest {
         @Test
         public void itHasNoNextElement() {
-            Deque<String> deque = new Deque<String>();
+            Deque<String> deque = new Deque<>();
             Iterator<String> it = deque.iterator();
             assertFalse(it.hasNext());
         }
 
         @Test(expected = NoSuchElementException.class)
         public void nextThrowsException() {
-            Deque<String> deque = new Deque<String>();
+            Deque<String> deque = new Deque<>();
             Iterator<String> it = deque.iterator();
             it.next();
         }
 
         @Test(expected = UnsupportedOperationException.class)
         public void removeThrowsException() {
-            Deque<String> deque = new Deque<String>();
+            Deque<String> deque = new Deque<>();
             Iterator<String> it = deque.iterator();
             it.remove();
         }
