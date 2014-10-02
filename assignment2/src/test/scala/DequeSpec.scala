5696b01e228c3dd1cbff33a26aba61055e93fe76
Alexander Korneev <akorneev@gmail.com>
2014-10-03 03:02:05 +0400

    Port tests to Scala

---
+++ assignment2/src/test/scala/DequeSpec.scala
@@ -0,0 +1,71 @@
+import java.util.NoSuchElementException
+
+import org.scalatest.{FunSpec, Matchers}
+
+class DequeSpec extends FunSpec with Matchers {
+  describe("A Deque") {
+    describe("when empty") {
+      it("should be really empty") {
+        val deque = new Deque[String]
+        deque should be('empty)
+      }
+      it("should have size 0") {
+        val deque = new Deque[String]
+        deque.size should equal(0)
+      }
+      it("should throw exception if `removeFirst` is called") {
+        val deque = new Deque[String]
+        intercept[NoSuchElementException] {
+          deque.removeFirst()
+        }
+      }
+      it("should throw exception if `removeLast` is called") {
+        val deque = new Deque[String]
+        intercept[NoSuchElementException] {
+          deque.removeLast()
+        }
+      }
+      it("should become nonempty after inserting element at front") {
+        val deque = new Deque[String]
+        deque.addFirst("test")
+        deque should not be ('empty)
+      }
+      it("should become nonempty after inserting element at rear") {
+        val deque: Deque[String] = new Deque[String]
+        deque.addLast("test")
+        deque should not be 'empty
+      }
+      it("should have size 1 after inserting element at front") {
+        val deque = new Deque[String]
+        deque.addFirst("test")
+        deque.size should equal(1)
+      }
+      it("should have size 1 after inserting element at rear") {
+        val deque = new Deque[String]
+        deque.addLast("test")
+        deque.size should equal(1)
+      }
+      describe("should have an empty iterator") {
+        it("should have no next element") {
+          val deque = new Deque[String]
+          val it = deque.iterator
+          it.hasNext should be(false)
+        }
+        it("should throw exception when `next()` is called") {
+          val deque = new Deque[String]
+          val it = deque.iterator
+          intercept[NoSuchElementException] {
+            it.next()
+          }
+        }
+        it("should throw exception when `remove()` is called") {
+          val deque = new Deque[String]
+          val it = deque.iterator
+          intercept[UnsupportedOperationException] {
+            it.remove()
+          }
+        }
+      }
+    }
+  }
+}
